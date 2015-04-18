9138a221a308443473051245200c8559ca9c4f62
Dan Clark <thedanclark@gmail.com>
2015-04-17 21:14:20 -0700

    Set up file I/O

--- perspectiveRemover.py
+++ perspectiveRemover.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 
 from sys import argv
+import png
 
+NEW_FILE_SUFFIX = "FIXED"
 
 def showUsage():
     print("Usage:", argv[0], "filename")
+
+def writeToFile(targetFilename, theWidth, theHeight, pixels):
+    """Expects pixels in boxed row flat pixel form"""
+    with open(targetFilename, 'wb') as f:
+        png.Writer(width=theWidth, height=theHeight).write(f, p) 
+
 
 if __name__ == "__main__":
     print("Helloo!")
@@ -13,9 +21,25 @@ if __name__ == "__main__":
         exit()
 
 
-    filename = argv[0]
-    print("Processing file:", filename)
+    theFilename = argv[1]
+    print("Processing file:", theFilename)
     
+    (w, h, p, m) = png.Reader(filename = theFilename).asRGB() 
+
+    print("w:", w)
+    print("h:", h)
+    #print("p len next:", len(next(p)))
+    #print("m len next:", len(next(p)))
+    #print("p next:", next(p))
+    #print("m next:", next(p))
 
 
+    splitFilename = theFilename.split('.')
+    newFilename= ".".join(splitFilename[:-1]) + "." + NEW_FILE_SUFFIX + \
+        "." + splitFilename[-1]
 
+    print("Saving new image as", newFilename)
+
+    writeToFile(newFilename, w, h, p)
+   
+     
